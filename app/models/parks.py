b5169e51dd3b27fbd35cbd4d8e28591b965e92a7
Anthony Fahden <afahden@gmail.com>
2021-12-14 13:18:11 -0800

    park model

---
+++ app/models/parks.py
@@ -0,0 +1,26 @@
+from .db import db
+
+
+class Park(db.Model):
+    __tablename__ = 'parks'
+
+    id= db.Column(db.Integer, primary_key=True)
+    park_name = db.Column(db.String(50), nullable=False)
+    lat = db.Column(db.Float(precision=32, asdecimal=True, decimal_return_scale=None))
+    long = db.Column(db.Float(precision=32, asdecimal=True, decimal_return_scale=None))
+    description = db.Column(db.String(255), nullable=False)
+    country = db.Column(db.String(40), nullable=False)
+    state = db.Column(db.String(20), nullable=False)
+
+    
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'park_name': self.park_name,
+            'lat': self.lat,
+            'long': self.long,
+            'description': self.description,
+            'country': self.country,
+            'state': self.state
+    }
