30d7e500e16dd849d8843a7c1c95177d29e1aaf0
Anthony Fahden <afahden@gmail.com>
2021-12-14 14:19:57 -0800

    parks model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,4 @@
 from .db import db
 from .user import User
 from .parks import Park
+from .hikes import Hike
---
+++ app/models/hikes.py
@@ -0,0 +1,30 @@
+from .db import db
+
+
+class Hike(db.Model):
+    __tablename__ = 'hikes'
+
+    id= db.Column(db.Integer, primary_key=True)
+    park_id = db.Column(db.Integer, db.ForeignKey('parks.id'), nullable=False)
+    lat = db.Column(db.Float(precision=32, asdecimal=True, decimal_return_scale=None), nullable=False)
+    long = db.Column(db.Float(precision=32, asdecimal=True, decimal_return_scale=None), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    length = db.Column(db.Integer, nullable=False)
+    eta = db.Column(db.Integer, nullable=False)
+    difficulty = db.Column(db.String(15), nullable=False)
+
+    parks = db.relationship('Park', back_populates='hikes')
+
+    
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'park_name': self.park_name,
+            'lat': self.lat,
+            'long': self.long,
+            'description': self.description,
+            'length': self.length,
+            'eta': self.eta,
+            'difficulty': self.difficulty
+    }
