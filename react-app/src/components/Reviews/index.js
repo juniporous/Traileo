21cf0138773654c389d377bc08dd0917b8fd1c9f
Anthony Fahden <afahden@gmail.com>
2021-12-16 12:09:37 -0800

    search results rendering in console, not yet on frontend

--- react-app/src/components/Reviews/index.js
+++ react-app/src/components/Reviews/index.js
@@ -1,8 +1,8 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { getReviews, deleteReview, addReview, updateReview } from '../../store/review'
 import UpdateReviewForm from '../UpdateReview'
-import { getHikes } from '../../store/hike'
+import { getHikes, searchHikes } from '../../store/hike'
 import "./Reviews.css"
 
 function Review() {
@@ -10,15 +10,25 @@ function Review() {
     const sessionUser = useSelector((state) => state.session.user)
     const userId = sessionUser.id
     const reviews = useSelector(state => Object.values(state.review))
-    const hikes = useSelector(state => Object.values(state.hike))
-    console.log('@#$%@#$%@#$%', hikes)
+    
+
+    const [search, setSearch] = useState('')
+    const hikeResult = useSelector(state => Object.values(state.hike))
+    useEffect(()=>{
+        dispatch(searchHikes(search))
+    }, [dispatch, search])
+    console.log('######', hikeResult)
+
+
     useEffect(() => {
         dispatch(getReviews())
     }, [dispatch])
 
-    useEffect(() => {
-        dispatch(getHikes())
-    }, [dispatch])
+    // to display hikes
+    // const hikes = useSelector(state => Object.values(state.hike))
+    // useEffect(() => {
+    //     dispatch(getHikes())
+    // }, [dispatch])
 
     const handleDelete = (id) => {
         dispatch(deleteReview(id));
@@ -53,13 +63,32 @@ function Review() {
                 ))}
             </div>
             <button onClick={() => handlePost()}>Post Review</button>
-            <div>
+            {/* <div>
                 {hikes.map(hike => (
                     <div key={hike.id}>
                         {hike.id} hike
                     </div>
                 ))}
+            </div> */}
+            <form>
+          <div className="SearchContainer">
+            <div className="pageTitle">
+              <div>Search</div>
             </div>
+            <input
+              className="subTitleSearch"
+              type="text"
+              value={search}
+              placeholder="Find a song or artist..."
+              onChange={(e) => setSearch(e.target.value)}
+            />{" "}
+            <span
+              className={search.length == 0 ? "hideText" : "subTitleSearch1"}
+            >
+              
+            </span>
+          </div>
+        </form>
         </>
     )
 }
--- react-app/src/store/hike.js
+++ react-app/src/store/hike.js
@@ -9,28 +9,27 @@ const showHikes = (data) => {
 
 export const getHikes = () => async dispatch => {
   const response = await fetch('/api/hikes/')
-    if (response.ok) {
-      const data = await response.json();
-      dispatch(showHikes(data));
-    }
+    const data = await response.json();
+    dispatch(showHikes(data));
+    
 }
 
-// export const searchHikes = (searchParam) => async dispatch => {
-//   const response = await fetch('/api/hikes/search', {
-//     method: 'POST',
-//     headers: {'Content-Type':'application/json'},
-//     body: JSON.stringify(searchParam)
-//   })
-//   const data = await response.json();
-//   dispatch(showHikes(data))
-// }
+export const searchHikes = (searchParam) => async dispatch => {
+  const response = await fetch('/api/hikes/search', {
+    method: 'POST',
+    headers: {'Content-Type':'application/json'},
+    body: JSON.stringify(searchParam)
+  })
+  const data = await response.json();
+  dispatch(showHikes(data))
+}
 
 const initialState = {};
 export default function reducer(state = initialState, action) {
   let newState = {};
   switch (action.type) {
     case GET_HIKES:
-      newState = {...state};
+      newState = {};
       action.data.hikes.forEach(hike => newState[hike.id] = hike)
       return newState;
     default:
