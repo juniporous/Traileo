760a9e48bf7cdb718a6edc99dd8bab261a8f78d6
Anthony Fahden <afahden@gmail.com>
2021-12-21 13:51:45 -0800

    photo size normalized on hike page

--- react-app/src/components/HikePhotos/index.js
+++ react-app/src/components/HikePhotos/index.js
@@ -3,17 +3,9 @@ import './HikePhotos.css'
 
 const HikePhotos = ({ hikeId, photos }) => {
     return (
-        <div>
+        <div className='photo-container'>
             {photos.map(photo => ( photo.hike_id == hikeId ?
-                <div key={photo.id}>
-                    <div class="card">
-                        <div class="container">
-                        <div>
-                           <img className='img' src={photo.img_url} alt='No Image For This Spot'/>
-                        </div>
-                        </div>
-                    </div>
-                </div>
+                    <img className='item' key={photo.id} src={photo.img_url} alt='No Image For This Spot'/>
                 : null
             ))
             }
