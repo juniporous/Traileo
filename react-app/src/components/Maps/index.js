bcd08fcc137b7105eb5f6eae584368671eef5c64
Anthony Fahden <afahden@gmail.com>
2021-12-24 23:20:19 -0800

    map renders pop-up cards on click, but also dotenv api key bug

--- react-app/src/components/Maps/index.js
+++ react-app/src/components/Maps/index.js
@@ -1,19 +1,29 @@
 import React from "react";
-import { useEffect } from "react";
-import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api'
+import { useEffect, useState } from "react";
+import { GoogleMap, useJsApiLoader, Marker, InfoWindow } from '@react-google-maps/api'
 
 
 const AllHikesMap = ({hikes}) => {
-
+    const [selectedCenter, setSelectedCenter] = useState(null);
     const { isLoaded } = useJsApiLoader({
         id: 'google-map-script',
-        googleMapsApiKey: process.env.REACT_GOOGLE_MAPS_API_KEY
+        googleMapsApiKey: process.env.REACT_APP_MAPS_API
     })
+
     const center={lat: 38.905065, lng: -120.098954}
     const containerStyle={height: '400px', width: '400px'}
+    useEffect(() => {
+        const listener = e => {
+           if (e.key === "Escape") {
+              setSelectedCenter(null);
+           }
+        };
+        window.addEventListener("keydown", listener);
+        return () => {
+           window.removeEventListener("keydown", listener);
+        };
+     }, []);
 
-
-    
     return (
         <>
             <div>
@@ -29,9 +39,28 @@ const AllHikesMap = ({hikes}) => {
                             position={{
                             lat: center.lat,
                             lng: center.long
-                            }} 
+                            }}
+                            
+                            onClick={() => {
+                                setSelectedCenter(center);
+                             }}
                         />
                     ))}
+
+                    {selectedCenter && (
+                    <InfoWindow
+                        onCloseClick={() => {
+                            setSelectedCenter(null);
+                        }}
+                        position={{
+                            lat: selectedCenter.lat,
+                            lng: selectedCenter.long
+                        }}
+                    >
+                        <div>{selectedCenter.hike_name}</div>
+                    </InfoWindow>
+                    )}
+
                 </GoogleMap>
                 )}
             </div>
