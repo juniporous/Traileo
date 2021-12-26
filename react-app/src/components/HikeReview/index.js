b2f47f1ceb3fe026eeeb60e27f8ebb8b865d5346
Anthony Fahden <afahden@gmail.com>
2021-12-25 20:44:37 -0800

    review content positioning

--- react-app/src/components/HikeReview/index.js
+++ react-app/src/components/HikeReview/index.js
@@ -3,24 +3,26 @@ import './HikeReview.css'
 
 const HikeReview = ({ hikeId, reviews }) => {
     return (
-        <ul className='ul'>
-            {reviews.map(review => ( review.hike_id == hikeId ?
-                <li key={review.id} className='li-review'>
-                    <div class="card">
-                        <div class="container">
-                            <div>
-                                Rating: {review.rating}
-                            </div>
-                            <div className="description-text">
-                                {review.description}
+        <div className='ul-container'>
+            <ul className='ul'>
+                {reviews.map(review => ( review.hike_id == hikeId ?
+                    <li key={review.id} className='li-review'>
+                        <div class="card">
+                            <div class="container">
+                                <div>
+                                    Rating: {review.rating}
+                                </div>
+                                <div className="description-text">
+                                    {review.description}
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </li>
-                : null
-            ))
-            }
-        </ul>
+                    </li>
+                    : null
+                ))
+                }
+            </ul>
+        </div>
     )
 }
 
