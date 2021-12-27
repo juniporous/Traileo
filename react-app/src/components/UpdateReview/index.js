10748248a755db4b5d91abb8decb1abdfeee5ff5
Anthony Fahden <afahden@gmail.com>
2021-12-27 15:36:40 -0800

    increase dashboard review spacing

--- react-app/src/components/UpdateReview/index.js
+++ react-app/src/components/UpdateReview/index.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { updateReview } from '../../store/review';
+import './UpdateReview.css'
 
 
 const UpdateReviewForm = ({ reviewId }) => {
