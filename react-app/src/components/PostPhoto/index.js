812f43998ce4eda1c5a24d8fb3f7c5ca72fff832
Anthony Fahden <afahden@gmail.com>
2021-12-28 09:31:09 -0800

    remove unused review components

--- react-app/src/components/PostPhoto/index.js
+++ react-app/src/components/PostPhoto/index.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { addPhoto } from '../../store/photo'
 import './PostPhoto.css'
 
